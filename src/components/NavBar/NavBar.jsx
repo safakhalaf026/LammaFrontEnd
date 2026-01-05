c658d854f3c1234d68648b0e43e37cb9a0ff9441
safakhalaf026 <safakhalaf026@gmail.com>
2026-01-05 14:37:13 +0300

    added ascaffolding

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router'
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+
+const NavBar = () => {
+  const {user, setUser} = useContext(UserContext)
+
+  const handleSignOut = ()=>{
+    localStorage.removeItem('token')
+    setUser(null)
+  }
+  return (
+    <nav>
+      {user ? (
+        <ul>
+          <li><Link to='/'>Dashboard</Link></li>
+          <li><Link onClick={handleSignOut} to='/'>Sign Out</Link></li>
+        </ul>) :
+        <ul>
+          <li><Link to='/'>Home</Link></li> {/* this leads to landing */}
+          <li><Link to='/sign-up'>Sign Up</Link></li>
+          <li><Link to='/sign-in'>Sign In</Link></li>
+        </ul>
+      }
+    </nav>
+  );
+};
+
+export default NavBar;
+
