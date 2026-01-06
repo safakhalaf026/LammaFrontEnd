4d0781307839ec804410c28d3c166a46fd1d9b41
twinsmaram-hub <twinsmaram@gmail.com>
2026-01-06 15:54:10 +0300

    add reset button and div position

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -47,15 +47,28 @@ const Dashboard = () => {
     // References the HTML div where the map will be drawn
     const mapContainerRef = useRef(null)
 
+    const [center, setCenter] = useState(INITIAL_CENTER)
+    const [zoom, setZoom] = useState(INITIAL_ZOOM)
 
     useEffect(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
     mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
+        center: center,
+        zoom: zoom,
         style: 'mapbox://styles/mapbox/standard',
-        center: INITIAL_CENTER, 
-        zoom: INITIAL_ZOOM
-        });
+
+  })
+    mapRef.current.on('move', () => {
+        // get the current center coordinates and zoom level from the map
+        const mapCenter = mapRef.current.getCenter()
+        const mapZoom = mapRef.current.getZoom()
+
+        // update state
+        setCenter([ mapCenter.lng, mapCenter.lat ])
+        setZoom(mapZoom)
+    })
+   
 
     return () => {
       mapRef.current.remove()
@@ -63,8 +76,18 @@ const Dashboard = () => {
   }, [])
 
 
-    return (
-        <>
+
+   
+const handleButtonClick = () => {
+  mapRef.current.flyTo({
+    center: INITIAL_CENTER,
+    zoom: INITIAL_ZOOM
+  })
+}
+
+
+return (
+ <>
         <main>
             <h1>Welcome, {user.username}</h1>
             <p>
@@ -73,12 +96,21 @@ const Dashboard = () => {
             <p><strong>{message}</strong></p>
         </main>
         <div>
+              
+                <button className='reset-button' onClick={handleButtonClick}>
+                Reset
+                </button>
+                <div className="sidebar">
+                Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+                </div>
+
             <div id='map-container' ref={mapContainerRef}/>
 
         </div>
         </>
-    )
+)
 }
 
+ 
 export default Dashboard
 
